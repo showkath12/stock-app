5753c442188561577fc93b43d04dd80e52c95311
showkath <shaikshoukath07@gmail.com>
2022-04-12 21:17:29 +0530

    code refactor changes

--- app/src/app/dashoard/dashoard.component.ts
+++ app/src/app/dashoard/dashoard.component.ts
@@ -13,6 +13,10 @@ export class DashoardComponent implements OnInit {
   newArr = [];
   token: any;
   data: any = [];
+  dummyArr:any = [];
+  finalObj:any = {};
+  someObj:any;
+
   constructor(private http: HttpClient, private router: Router,) {
     this.token = 'bu4f8kn48v6uehqi3cqg';
     localStorage.setItem("apiKey", this.token);
@@ -34,28 +38,52 @@ export class DashoardComponent implements OnInit {
         let api = `${baseUrl}/quote?symbol=${element}&token=${api_key}`;
         let searchApi = `${baseUrl}/search?q=${element}&token=${api_key}`;
         this.http.get(api).subscribe(
-          res => {
+          (res: any) => {
+            let obj = {};
+            obj = {
+              ...res,
+              'symbol': element
+            }
             this.http.get(searchApi).subscribe((resp: any) => {
-              this.data.push(res);
+                  this.finalObj = {
+                    ...obj,
+                    'description': resp.result[0]?.description
+                  }
+                this.data.push(this.finalObj);
             });
           });
       });
     } else if (value) {
       let api = `${baseUrl}/quote?symbol=${value}&token=${api_key}`;
+      let searchApi = `${baseUrl}/search?q=${value}&token=${api_key}`;
       this.http.get(api).subscribe(
-        res => {
-          this.data.push(res);
+        (res: any) => {
+          let obj = {};
+          obj = {
+            ...res,
+            'symbol': value
+          }        
+          console.log(obj)
+          this.http.get(searchApi).subscribe((resp: any) => {
+                this.finalObj = {
+                  ...obj,
+                  'description': resp.result[0]?.description
+                }
+              this.data.push(this.finalObj);
+          });
         });
     }
   }
 
   // Adding stocks to local storage
   storeData() {
-    this.newArr = this.newArr || [];
-    this.newArr.push(this.convertToUpperCase(this.enteredValue));
-    localStorage.setItem("stocks", JSON.stringify(this.newArr));
-    this.getStockData(this.convertToUpperCase(this.enteredValue));
-    this.enteredValue = "";
+    if (this.enteredValue) {
+      this.newArr = this.newArr || [];
+      this.newArr.push(this.convertToUpperCase(this.enteredValue));
+      localStorage.setItem("stocks", JSON.stringify(this.newArr));
+      this.getStockData(this.convertToUpperCase(this.enteredValue));
+      this.enteredValue = "";
+    }
   }
 
   // By default converting to uppercase
@@ -70,7 +98,7 @@ export class DashoardComponent implements OnInit {
   }
 
   // Navigate to component
-  redirectTo() {
-    this.router.navigate(['sentiment' + `/TSLA`]);
+  redirectTo(item) {
+    this.router.navigate(['sentiment' + `/${item.symbol}`]);
   }
 }
